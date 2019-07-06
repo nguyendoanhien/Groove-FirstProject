fe8236e43119853ee1b5d6886621e224fb6019ca
NGUYEN CONG HAU <15110043@student.hcmute.edu.vn>
2019-07-06 22:15:21 +0700

    Commit GM_ClientSide (+chat theme)

---
+++ GM_ClientSide/src/app/app.module.ts
@@ -0,0 +1,74 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
+import { MatMomentDateModule } from '@angular/material-moment-adapter';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { TranslateModule } from '@ngx-translate/core';
+import 'hammerjs';
+
+import { FuseModule } from '@fuse/fuse.module';
+import { FuseSharedModule } from '@fuse/shared.module';
+import { FuseProgressBarModule, FuseSidebarModule, FuseThemeOptionsModule } from '@fuse/components';
+
+import { fuseConfig } from 'app/fuse-config';
+
+import { AppComponent } from 'app/app.component';
+import { LayoutModule } from 'app/layout/layout.module';
+// services
+import {ChatService} from './apps/chat/chat.service';
+import { FakeDbService } from './fake-api/fake-api.service';
+const appRoutes: Routes = [
+    {
+        path        : 'apps',
+        loadChildren: './apps/apps.module#AppsModule'
+    },
+    {
+        path      : '**',
+        redirectTo: './apps/apps.module#AppsModule'
+    }
+];
+
+@NgModule({
+    declarations: [
+        AppComponent
+    ],
+    imports     : [
+        BrowserModule,
+        BrowserAnimationsModule,
+        HttpClientModule,
+        RouterModule.forRoot(appRoutes),
+
+        TranslateModule.forRoot(),
+        InMemoryWebApiModule.forRoot(FakeDbService, {
+            delay             : 0,
+            passThruUnknownUrl: true
+        }),
+        // Material moment date module
+        MatMomentDateModule,
+
+        // Material
+        MatButtonModule,
+        MatIconModule,
+
+        // Fuse modules
+        FuseModule.forRoot(fuseConfig),
+        FuseProgressBarModule,
+        FuseSharedModule,
+        FuseSidebarModule,
+        FuseThemeOptionsModule,
+
+        // App modules
+        LayoutModule
+    ],
+    bootstrap   : [
+        AppComponent
+    ],
+    providers:[ChatService]
+})
+export class AppModule
+{
+}
