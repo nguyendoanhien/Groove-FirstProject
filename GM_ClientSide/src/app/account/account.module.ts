8de8f5b8864cef4b498bae5f0b123fd4b0cb7ff6
haunguyen97 <hau.nguyen@groovetechnology.com>
2019-07-08 13:52:45 +0700

    Updated theme (include login, register, forgot pass, reset pass themes)

--- GM_ClientSide/src/app/account/account.module.ts
+++ GM_ClientSide/src/app/account/account.module.ts
@@ -1,21 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AccountRoutingModule } from './account-routing.module';
-import { LoginComponent } from './login/login.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
-import { MatFormFieldModule, MatInputModule, MatIconModule, MatButtonModule  } from '@angular/material';
-import { FormsModule } from '@angular/forms';
-
-const matModules = [MatFormFieldModule, MatInputModule, MatIconModule, MatButtonModule ];
+import { MatFormFieldModule, MatInputModule, MatIconModule, MatButtonModule } from '@angular/material';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import {ForgotPasswordComponent} from './forgot-password/forgot-password.component';
+import {ResetPasswordComponent} from './reset-password/reset-password.component'
+const matModules = [MatFormFieldModule, MatInputModule, MatIconModule, MatButtonModule, MatCheckboxModule];
 
 
 @NgModule({
-  declarations: [LoginComponent, UserProfileComponent],
+  declarations: [LoginComponent,RegisterComponent,ResetPasswordComponent,ForgotPasswordComponent, UserProfileComponent],
   imports: [
     CommonModule,
     AccountRoutingModule,
     matModules,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class AccountModule { }
--- GM_ClientSide/src/app/account/login/login.component.ts
+++ GM_ClientSide/src/app/account/login/login.component.ts
@@ -1,23 +1,62 @@
-import { Component, OnInit } from '@angular/core';
-import { LoginModel } from './login.model';
-import { UserProfileService } from 'app/core/identity/userprofile.service';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { FuseConfigService } from '@fuse/services/config.service';
+import { fuseAnimations } from '@fuse/animations';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+    selector     : 'login',
+    templateUrl  : './login.component.html',
+    styleUrls    : ['./login.component.scss'],
+    encapsulation: ViewEncapsulation.None,
+    animations   : fuseAnimations
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit
+{
+    loginForm: FormGroup;
 
-  private login = new LoginModel('cuong.duongduy@gmail.com', 'Qwerty@123');
+    /**
+     * Constructor
+     *
+     * @param {FuseConfigService} _fuseConfigService
+     * @param {FormBuilder} _formBuilder
+     */
+    constructor(
+        private _fuseConfigService: FuseConfigService,
+        private _formBuilder: FormBuilder
+    )
+    {
+        // Configure the layout
+        this._fuseConfigService.config = {
+            layout: {
+                navbar   : {
+                    hidden: true
+                },
+                toolbar  : {
+                    hidden: true
+                },
+                footer   : {
+                    hidden: true
+                },
+                sidepanel: {
+                    hidden: true
+                }
+            }
+        };
+    }
 
-  constructor(private userProfileService: UserProfileService) { }
+    // -----------------------------------------------------------------------------------------------------
+    // @ Lifecycle hooks
+    // -----------------------------------------------------------------------------------------------------
 
-  ngOnInit(): void {
-  }
-
-  onSubmit(): void {
-    this.userProfileService.logIn(this.login);
-  }
-
+    /**
+     * On init
+     */
+    ngOnInit(): void
+    {
+        this.loginForm = this._formBuilder.group({
+            email   : ['', [Validators.required, Validators.email]],
+            password: ['', Validators.required]
+        });
+    }
 }
